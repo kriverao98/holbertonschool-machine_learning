e754cb9d6d89aa7358d8bbd48ebaa688da1f235f
kriverao98 <riveraocasiokoral29@gmail.com>
2024-12-07 00:45:36 -0400

    Add docstring for matrix transpose function in 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-
+"""
+Transposes a given 2D matrix.
+"""
 
 def matrix_transpose(matrix):
+    """
+    Transposes a given 2D matrix.
+
+    Args:
+        matrix (list of list of int/float): The matrix to be transposed.
+
+    Returns:
+        list of list of int/float: The transposed matrix.
+    """
     return [[matrix[j][i] for j in range(len(matrix))]
             for i in range(len(matrix[0]))]
