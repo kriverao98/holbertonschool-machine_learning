2d9126b4fc39fdd0299bd263a28e76bbdd22cd67
kriverao98 <riveraocasiokoral29@gmail.com>
2024-12-08 04:37:18 -0400

    Add matrix transposition functionality using NumPy in 11-the_western_exchange.py

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Transposes the given matrix using NumPy's transpose function."""
+
+
+def np_transpose(matrix):
+    """
+    Transposes the given matrix using NumPy's transpose function.
+
+    Parameters:
+    matrix (numpy.ndarray): The matrix to be transposed.
+
+    Returns:
+    numpy.ndarray: The transposed matrix.
+    """
+    return matrix.transpose()
