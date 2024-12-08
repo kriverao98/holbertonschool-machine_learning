344cdbc0d7ecf8b4abd2dfd9bb42a95a587772cd
kriverao98 <riveraocasiokoral29@gmail.com>
2024-12-08 04:31:21 -0400

    Add function to return the shape of a numpy matrix in 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Returns the shape of a numpy matrix."""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy matrix.
+
+    Parameters:
+    matrix (numpy.ndarray): The matrix to get the shape of.
+
+    Returns:
+    tuple: A tuple representing the shape of the matrix.
+    """
+    return matrix.shape
