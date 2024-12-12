148a20823e66a724be12b39f3f451a9dd1ff9805
kriverao98 <riveraocasiokoral29@gmail.com>
2024-12-11 21:17:40 -0400

    Add line graph plotting functionality for y = x^3

---
+++ math/plotting/0-line.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+"""Plots a line graph of y = x^3 for x in the range [0, 10]."""
+
+def line():
+    """
+    Plots a line graph of y = x^3 for x in the range [0, 10].
+
+    The function creates a plot with the y-values being the cube of numbers from 0 to 10.
+    The plot is displayed with a red line for the y-axis values and the x-axis ranging from 0 to 10.
+    The figure size is set to 6.4 by 4.8 inches.
+    """
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Y-axis red line
+    plt.plot(y, color='red')
+
+    # X-axis ranging from 0 - 10
+    plt.xlim(0, 10)
+
+    # Render plot
+    plt.show()
