0a4a3408bbbc98ecf2291d7cac09ec4036b87ff5
kriverao98 <riveraocasiokoral29@gmail.com>
2024-12-12 21:01:54 -0400

    Set x and y limits for histogram plot to enhance visualization

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -24,6 +24,8 @@ def frequency():
     bins = range(0, 101, 10)
 
     plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
 
     plt.title('Project A')
     plt.xlabel('Grades')
