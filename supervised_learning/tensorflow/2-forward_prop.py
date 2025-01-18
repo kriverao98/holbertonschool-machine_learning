207c66c637d9c81e61ba86193866fe0bf7357c73
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-18 11:39:21 -0400

    Refactor forward propagation function docstring for improved readability

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -4,14 +4,17 @@ import tensorflow.compat.v1 as tf
 
 create_layer = __import__('1-create_layer').create_layer
 
+
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
     Performs forward propagation for a neural network.
 
     Args:
         x: tf.placeholder, the input data.
-        layer_sizes: list of integers, the number of nodes in each layer of the network.
-        activations: list of activation functions for each layer of the network.
+        layer_sizes: list of integers, the number of nodes
+        in each layer of the network.
+        activations: list of activation functions for each layer
+        of the network.
 
     Returns:
         The output of the final layer after forward propagation.
