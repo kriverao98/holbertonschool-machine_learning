6d15161b0bd95122bdaf8d04a8b5f8734992fffe
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-18 11:47:40 -0400

    Add accuracy calculation function and main script for neural network evaluation

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Calculates the accuracy of a prediction."""
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Calculates the accuracy of a prediction.
+
+    Args:
+    y: tf.Tensor - placeholder for the labels of the input data
+    y_pred: tf.Tensor - tensor containing the network’s predictions
+
+    Returns:
+    tf.Tensor - tensor containing the decimal accuracy of the prediction
+    """
+    correct_predictions = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    return accuracy
