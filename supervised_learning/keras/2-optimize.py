bde45d831552d0b19dee5706b20edc58e116e9e3
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-19 17:37:36 -0400

    Refactor optimize_model function documentation for clarity and detail

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python3
 """
-Defines a function that sets up Adam optimizer for Keras model
-with categorical crossentropy loss and accuracy metrics
+    Keras project (finally)
 """
-
-
 import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
+    """ Sets up Adam optimization for keras model with categorical
+        crossentropy loss and accuracy metrics
+        network is model to optimize
+        alpha is learning rate
+        beta1 is first Adam optimization parameter
+        beta2 is second adam optmization parameter
+        Returns: None
     """
-    Sets up Adam optimizer for Keras model with
-        categorical crossentropy loss and accuracy metrics
-
-    parameters:
-        network [keras model]: model to optimize
-        alpha [float]: learning rate
-        beta1: first Adam optimization parameter
-        beta2: second Adam optimization parameter
-
-    returns:
-        None
-    """
-    network.compile(optimizer=K.optimizers.Adam(lr=alpha,
-                                                beta_1=beta1,
-                                                beta_2=beta2),
-                    loss='categorical_crossentropy',
+    A = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(loss='categorical_crossentropy', optimizer=A,
                     metrics=['accuracy'])
-    return None
