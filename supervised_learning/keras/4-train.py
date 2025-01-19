d472b25774fc64c38127d2e86e53343357fd6a08
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-19 17:43:17 -0400

    Add docstring to train_model function for clarity on training process

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent."""
 import tensorflow.keras as K
+
 
 def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
     """
