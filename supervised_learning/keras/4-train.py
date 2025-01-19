55a7ff4da2d906adc8a3d4845f534b66f0ed2eff
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-19 17:04:12 -0400

    Refactor train_model function documentation for clarity and consistency

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python3
-"""
-Defines a function that trains a model using mini-batch gradient descent
-using Keras library
-"""
-
-
+"""Trains a model using mini-batch gradient descent."""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """
-    Trains a model using mini-batch gradient descent with Keras
+    Trains a model using mini-batch gradient descent.
 
-    parameters:
-        network [keras model]: model to train
-        data [numpy.ndarray of shape (m, nx)]:
-            contains the input data
-        labels [one-hot numpy.ndarray of shape (m, classes)]:
-            contains labels of data
-        batch_size [int]:
-            size of batch used for mini-batch gradient descent
-        epochs [int]:
-            number of passes through data for mini-batch gradient descent
-        verbose [boolean]:
-            determines if output should be printed during training
-        shuffle [boolean]:
-            determines whether to shuffle the batches every epoch
+    Parameters:
+    - network: the model to train
+    - data: numpy.ndarray of shape (m, nx) containing the input data
+    - labels: one-hot numpy.ndarray of shape
+    (m, classes) containing the labels of data
+    - batch_size: size of the batch used for mini-batch gradient descent
+    - epochs: number of passes through data for mini-batch gradient descent
+    - verbose: boolean that determines if output
+    should be printed during training
+    - shuffle: boolean that determines whether to
+    shuffle the batches every epoch
 
-    returns:
-        the History object generated after training the model
+    Returns:
+    - History object generated after training the model
     """
-    history = network.fit(x=data, y=labels,
-                          batch_size=batch_size,
-                          epochs=epochs,
-                          verbose=verbose,
-                          shuffle=shuffle)
+    history = network.fit(data, labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle)
     return history
