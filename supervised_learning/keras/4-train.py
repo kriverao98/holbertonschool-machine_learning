d5403d407959233cb56ca0298b49a5c10ac31168
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-19 17:45:14 -0400

    Refactor train_model function docstring for improved readability and formatting

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,21 +3,26 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
     """
     Trains a model using mini-batch gradient descent.
 
     Parameters:
     - network: the model to train
     - data: numpy.ndarray of shape (m, nx) containing the input data
-    - labels: one-hot numpy.ndarray of shape (m, classes) containing the labels of data
+    - labels: one-hot numpy.ndarray of shape
+    (m, classes) containing the labels of data
     - batch_size: size of the batch used for mini-batch gradient descent
     - epochs: number of passes through data for mini-batch gradient descent
-    - verbose: boolean that determines if output should be printed during training
-    - shuffle: boolean that determines whether to shuffle the batches every epoch
+    - verbose: boolean that determines if output
+    should be printed during training
+    - shuffle: boolean that determines whether to
+    shuffle the batches every epoch
 
     Returns:
     - History object generated after training the model
     """
-    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle)
+    history = network.fit(data, labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle)
     return history
