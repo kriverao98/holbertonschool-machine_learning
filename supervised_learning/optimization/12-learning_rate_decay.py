e13efb9861d361e287d5c9b82f9b2a2495982ecc
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-26 14:16:25 -0400

    Refactor learning rate decay function to simplify parameters and improve documentation

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -5,19 +5,17 @@
 import tensorflow as tf
 
 
-def learning_rate_decay(alpha, decay_rate,
-                        global_step, decay_step):
+def learning_rate_decay(alpha, decay_rate, decay_step):
     """ creates learning rate decay operation in tensorflow
         using inverse time decay
-        learning_rate_decay(alpha_init, 1, i, 10)
 
         alpha is the original learning rate
-        decay_rate is the weight dermining alpha decay
-        global_step is number of gradient descent passes elapsed
-        decay_step is number of passes to occur before alpha decayed again
+        decay_rate is the weight used to determine the rate at which alpha will decay
+        decay_step is the number of passes of gradient descent that should occur before alpha is decayed further
 
-        Returns: updated value for alpha
+        Returns: the learning rate decay operation
     """
-    LRD = tf.train.inverse_time_decay(alpha, global_step, decay_step,
-                                      decay_rate, staircase=True)
-    return LRD
+    global_step = tf.Variable(0, trainable=False, name='global_step')
+    learning_rate = tf.compat.v1.train.inverse_time_decay(alpha, global_step, decay_step,
+                                                          decay_rate, staircase=True)
+    return learning_rate
