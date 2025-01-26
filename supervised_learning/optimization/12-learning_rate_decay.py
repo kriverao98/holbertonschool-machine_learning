f330f9b9126b123d8b76abd0e96dad68c6450f95
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-26 14:21:14 -0400

    Refactor learning rate decay function to improve variable naming for clarity

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -15,7 +15,7 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
 
         Returns: the learning rate decay operation
     """
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-    learning_rate = tf.compat.v1.train.inverse_time_decay(alpha, global_step, decay_step,
-                                                          decay_rate, staircase=True)
-    return learning_rate
+    LRD = tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                         decay_rate, staircase=True)
+
+    return LRD
