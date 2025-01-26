62db235fac4a1a1491e3ecf2ddb8692a1e072ccd
kriverao98 <riveraocasiokoral29@gmail.com>
2025-01-26 02:10:57 -0400

    Refactor Adam optimizer setup to remove loss parameter and update docstring

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -4,21 +4,19 @@
 import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+def create_Adam_op(alpha, beta1, beta2, epsilon):
     """
-    Function to train a DNN with TF RMSProp optimization
+    Function to set up the Adam optimization algorithm in TensorFlow
     Args:
-        loss: loss of the network
         alpha: learning rate
         beta1: weight used for the first moment
         beta2: weight used for the second moment
         epsilon: small number to avoid division by zero
 
-    Returns: Adam optimization operation
-
+    Returns: Adam optimizer
     """
-    optimizer = tf.train.AdamOptimizer(alpha,
-                                       beta1,
-                                       beta2,
-                                       epsilon).minimize(loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha,
+                                       beta1=beta1,
+                                       beta2=beta2,
+                                       epsilon=epsilon)
     return optimizer
